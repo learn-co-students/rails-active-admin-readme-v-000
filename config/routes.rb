27eb600e0d16314fcda9f1e011b87dd7a9a7ea7c
Jacob <mailme711@gmail.com>
2016-04-19 15:33:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :authors, except: [:delete]
