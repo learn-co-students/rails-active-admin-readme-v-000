319968f71c089ff75948e5950171ceb4ce8c916c
github username <github email address>
2016-11-07 12:09:15 -0500

    Done.

---
+++ app/admin/author.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Author do
+
+  permit_params :name, :genre
+  actions :all, except: [:destroy]
+
+  form do |f|
+    inputs 'Author' do
+      f.input :name
+      f.input :genre
+    end
+    f.semantic_errors
+    f.actions
+  end
+
+
+end
