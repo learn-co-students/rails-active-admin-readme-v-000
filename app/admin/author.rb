c6d4b1b3ae0e9a0453374f51c35cf777bf3dd246
github username <jilustrisimo@users.noreply.github.com>
2017-04-05 22:23:20 -0400

    Done.

---
+++ app/admin/author.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Author do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+  permit_params :name, :genre
+  actions :all, except: [:destroy]
+
+  form do |f|
+    inputs 'Author' do
+      f.input :name
+      f.input :genre
+    end
+    f.semantic_errors
+    f.actions
+  end
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
