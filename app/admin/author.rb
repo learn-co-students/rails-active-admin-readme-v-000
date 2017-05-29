4c2bb755fd7b3c1e2b1e47eed6d95ec664749495
kkterai <kkterai@gmail.com>
2017-05-29 12:41:24 -0700

    Done.

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -12,14 +12,14 @@ ActiveAdmin.register Author do
   end
 
 
-  form do |f|
-    f.inputs "Author" do
-      f.input :name
-      f.input :genre
-    end
-    f.semantic_errors
-    f.actions
-  end
+  # form do |f|
+  #   f.inputs "Author" do
+  #     f.input :name
+  #     f.input :genre
+  #   end
+  #   f.semantic_errors
+  #   f.actions
+  # end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
