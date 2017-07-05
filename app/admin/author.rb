734385476154808c680f60501cba1f76012b62dc
Hui Jiang <hui1021@yahoo.com>
2017-07-05 20:33:39 +0000

    Done.

---
+++ app/admin/author.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Author do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+    permit_params :name, :genre
+    actions :all, except: [:destroy]
+    form do |f|
+      inputs 'Author' do
+        f.input :name
+        f.input :genre
+      end
+      f.semantic_errors
+      f.actions
+    end
+end
